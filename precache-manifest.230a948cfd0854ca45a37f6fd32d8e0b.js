320b1e85818cc1c6107c1b3bde5625d2d652814d
Aditya-cyber007 <aditya.singh_cs.aiml19@gla.ac.in>
2022-06-17 20:30:40 +0530

    Updates

---
+++ precache-manifest.230a948cfd0854ca45a37f6fd32d8e0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f7f97cca5ce645a08b5",
+    "url": "/TextAnalyzer/static/css/main.491ba0c8.chunk.css"
+  },
+  {
+    "revision": "3f7f97cca5ce645a08b5",
+    "url": "/TextAnalyzer/static/js/main.3f7f97cc.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextAnalyzer/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "fea4a0829c121c975406",
+    "url": "/TextAnalyzer/static/js/2.fea4a082.chunk.js"
+  },
+  {
+    "revision": "12536a27311aff5b4bb0",
+    "url": "/TextAnalyzer/static/js/runtime~main.12536a27.js"
+  },
+  {
+    "revision": "a39404e4014c931c9d5a6fd46396d6dd",
+    "url": "/TextAnalyzer/index.html"
+  }
+];
