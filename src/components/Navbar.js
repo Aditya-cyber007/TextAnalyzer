74784d742479a42bae045c2b8336ed203d9245e8
Aditya-cyber007 <aditya.singh_cs.aiml19@gla.ac.in>
2022-06-18 21:06:10 +0530

    navbarSopported component updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar(props) {
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="./navbarSupportedContent"
+          data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
